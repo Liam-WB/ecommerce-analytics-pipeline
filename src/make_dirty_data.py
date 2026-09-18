e78d11934f36058da72666273684a484062722b2
Liam <126467141+Liam-WB@users.noreply.github.com>
2026-09-18 16:19:27 +0000

    Add make_dirty_data.py functions

--- src/make_dirty_data.py
+++ src/make_dirty_data.py
@@ -51,6 +51,31 @@ def remove_emails(customers, percentage=0.02):
     return customers
 
 
+def duplicate_emails(customers, percentage=0.01):
+    number_to_modify = int(len(customers) * percentage)
+
+    selected_customers = random.sample(customers, number_to_modify)
+
+    for customer in selected_customers:
+        other_customer = random.choice(customers)
+
+        customer["email"] = other_customer["email"]
+
+    return customers
+
+
+def change_country_name(customers, percentage=0.02):
+    number_to_modify = int(len(customers) * percentage)
+
+    selected_customers = random.sample(customers, number_to_modify)
+
+    for customer in selected_customers:
+        if customer["country"] == "UK":
+            customer["country"] = "United Kingdom"
+
+    return customers
+
+
 def save_customers(customers, filepath):
     with open(filepath, "w", newline="", encoding="utf-8") as file:
         fieldnames = customers[0].keys()
@@ -71,6 +96,8 @@ def main():
     add_whitespace(customers)
     change_country_case(customers)
     remove_emails(customers)
+    duplicate_emails(customers)
+    change_country_name(customers)
 
     save_customers(customers, OUTPUT_FILE)
 
